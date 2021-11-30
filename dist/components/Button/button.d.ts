22331cd042e23e3728cf10cfc973afcacc87b323
rodchen <rodchen_king@outlook.com>
2021-11-30 21:55:20 +0800

    1. 优化结构，发布准备

---
+++ dist/components/Button/button.d.ts
@@ -0,0 +1,24 @@
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react';
+export declare enum ButtonSize {
+    Large = "lg",
+    Small = "sm"
+}
+export declare enum ButtonType {
+    Primary = "primary",
+    Default = "default",
+    Danger = "danger",
+    Link = "link"
+}
+interface BaseButtonProps {
+    className?: string;
+    disabled?: boolean;
+    size?: ButtonSize;
+    btnType?: ButtonType;
+    children: React.ReactNode;
+    href?: string;
+}
+declare type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>;
+declare type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>;
+export declare type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>;
+export declare const Button: FC<ButtonProps>;
+export default Button;
