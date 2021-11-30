f915e9166f70d29b4b27b6a16989a6ad9daa8337
rodchen <rodchen_king@outlook.com>
2021-11-30 14:57:29 +0800

    1. 完成button功能

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@
  * @Description: 
  * @Author: rodchen
  * @Date: 2021-11-29 23:21:32
- * @LastEditTime: 2021-11-30 01:34:07
+ * @LastEditTime: 2021-11-30 14:51:14
  * @LastEditors: rodchen
  */
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/buttons'
 
 function App() {
   return (
@@ -13,7 +14,15 @@ function App() {
       <header className="App-header">
         <h1>h1</h1>
         <h2>h2</h2>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}>large primary</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large} disabled>disable large primary</Button>
 
+        <Button btnType={ButtonType.Default}>default</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}>danger</Button>
+        <Button btnType={ButtonType.Link} href="w">link</Button>
+        <Button btnType={ButtonType.Link} disabled href="w">disable link</Button>
+
+        
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
---
+++ src/components/Button/buttons.tsx
@@ -0,0 +1,82 @@
+/*
+ * @Description: 
+ * @Author: rodchen
+ * @Date: 2021-11-30 10:01:19
+ * @LastEditTime: 2021-11-30 14:52:36
+ * @LastEditors: rodchen
+ */
+import React, { FC, ButtonHTMLAttributes, AnchorHTMLAttributes } from 'react'
+import classNames from 'classnames'
+
+export enum ButtonSize {
+  Large = 'lg',
+  Small = 'sm'
+}
+
+export enum ButtonType {
+  Primary = 'primary',
+  Default = 'default',
+  Danger = 'danger',
+  Link = 'link'
+}
+
+interface BaseButtonProps {
+  className?: string;
+  /**设置 Button 的禁用 */
+  disabled?: boolean;
+  /**设置 Button 的尺寸 */
+  size?: ButtonSize;
+  /**设置 Button 的类型 */
+  btnType?: ButtonType;
+  children: React.ReactNode;
+  href?: string;
+}
+type NativeButtonProps = BaseButtonProps & ButtonHTMLAttributes<HTMLElement>
+type AnchorButtonProps = BaseButtonProps & AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonProps & AnchorButtonProps>
+
+export const Button: FC<ButtonProps> = (props) => {
+  const { 
+    btnType,
+    className,
+    disabled,
+    size,
+    children,
+    href,
+    ...restProps
+  } = props
+  // btn, btn-lg, btn-primary
+  const classes = classNames('btn', className, {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    'disabled': (btnType === 'link') && disabled
+  })
+  if (btnType === 'link' && href ) {
+    return (
+      <a
+        className={classes}
+        href={href}
+        {...restProps}
+      >
+        {children}
+      </a>
+    )
+  } else {
+    return (
+      <button
+        className={classes}
+        disabled={disabled}
+        {...restProps}
+      >
+        {children}
+      </button>
+    )
+  }
+}
+
+Button.defaultProps = {
+  disabled: false,
+  btnType: ButtonType.Default
+}
+
+export default Button;
